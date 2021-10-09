f836d9a622ecb38b34e96f00e8b097810331546c
Antoine T-S <anto291100@gmail.com>
2021-10-08 23:27:13 -0400

    je push pour que thomas le test jai peur

--- Assets/Scripts/Cloaking.cs
+++ Assets/Scripts/Cloaking.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class Cloaking : MonoBehaviour
 {
-    public Material cloak;
-    private Material baseMaterial;
+    public static bool cloakUnlocked = false;
     private int chargesLeft;
+    private bool cloakToggled;
     private PlayerControls pC;
     private SpriteRenderer sR;
     // Start is called before the first frame update
     void Start()
     {
+        cloakToggled = false;
         sR = GetComponent<SpriteRenderer>();
-        baseMaterial = sR.material;
         pC = GetComponent<PlayerControls>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cloakUnlocked)
+        {
+            if (pC.GetManette().bButton.wasPressedThisFrame)
+            {
+                ToggleCloak();
+            }
+        }
         
+    }
+
+    void ToggleCloak()
+    {
+        if (!cloakToggled && chargesLeft > 0)
+        {
+            StartCoroutine(Cloak());
+        }
+    }
+
+    IEnumerator Cloak()
+    {
+        cloakToggled = true;
+        pC.lockMovement = true;
+        sR.color = new Color(sR.color.r, sR.color.g, sR.color.b, 0.49f);
+        while (!pC.GetManette().bButton.wasPressedThisFrame)
+        {
+            yield return null;
+        }
+        sR.color = new Color(sR.color.r, sR.color.g, sR.color.b, 1);
+        chargesLeft--;
+        cloakToggled = false;
+        pC.lockMovement = false;
+
+
     }
 }
