ad6b7a7f479e3b5e81b1d06c2c422f9c87937c17
Antoine T-S <anto291100@gmail.com>
2021-10-10 12:16:30 -0400

    oopsy

--- Assets/Crack.cs
+++ Assets/Crack.cs
@@ -88,7 +88,7 @@ public class Crack : MonoBehaviour
             yield return null;
         }
         canvas.SetActive(false);
-        if (monster.transform.position.x < -23f)
+        if (monster.transform.position.x > -23f)
         {
             SoundPlayer.instance.PlaySFX(Resources.Load<AudioClip>("Sound/SFX/FitCrack"), 1);
             FindObjectOfType<CinemachineVirtualCamera>().GetComponent<CameraShake>().ShakeCam(1f, 5f, 5f, RumbleForce.Weak);
