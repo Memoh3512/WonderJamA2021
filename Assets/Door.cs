39fa9aa6a904fa696ef5794e66a918abdbb0e0f4
Memoh <CraftMan3512@gmail.com>
2021-10-10 02:34:11 -0400

    fix everything wrong with merge and stuff

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -25,7 +25,7 @@ public class Door : MonoBehaviour
             DoorLockedText();
         }
 
-        player.GetComponent<PlayerControls>().lockMovement = false;
+        //player.GetComponent<PlayerControls>().lockMovement = false;
     }
 
     void OpenDoor(GameObject player)
