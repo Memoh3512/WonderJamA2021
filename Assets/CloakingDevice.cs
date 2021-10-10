b03a7cd69eec203edb0d1e8559bbcabe7744d82b
Memoh <CraftMan3512@gmail.com>
2021-10-10 14:01:20 -0400

    fix font

--- Assets/CloakingDevice.cs
+++ Assets/CloakingDevice.cs
@@ -7,7 +7,8 @@ public class CloakingDevice : MonoBehaviour
     public void PickUpCloakingDevice(GameObject player)
     {
         player.GetComponent<Cloaking>().UnlockCloak();
-        GameObject.Find("UIText").GetComponent<UIText>().DisplayText("You picked up a cloaking device (press F on/off or B for controllers)");
+        SoundPlayer.instance.PlaySFX(Resources.Load<AudioClip>("Sound/SFX/Pickup item_V01"), 1.5f);
+        GameObject.Find("UIText").GetComponent<UIText>().DisplayText("You picked up a cloaking device (F/B button to turn invisible). Has 3 charges",8f);
         Destroy(gameObject);
     }
 }
