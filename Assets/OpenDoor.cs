2adf0c9b069ce686a08a5b317d28c784d41cff94
Mattys Gervais <mattysgervais@gmail.com>
2021-10-10 10:37:43 -0400

    Doors are poggies

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class OpenDoor : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class OpenDoor : MonoBehaviour
     public Sprite opened;
     private GameObject closedCollider;
     private GameObject openedCollider;
+    public Light2D green;
+    public Light2D red;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +30,8 @@ public class OpenDoor : MonoBehaviour
         GameObject.FindWithTag("Player").GetComponent<PlayerControls>().lockMovement = false;
         openedCollider.GetComponent<BoxCollider2D>().enabled=true;
         closedCollider.GetComponent<BoxCollider2D>().enabled=false;
+        green.enabled = true;
+        red.enabled = false;
         Invoke("CloseSesame",2f);
         
     }
@@ -36,5 +41,7 @@ public class OpenDoor : MonoBehaviour
         GameObject.FindWithTag("Player").GetComponent<PlayerControls>().lockMovement = false;
         openedCollider.GetComponent<BoxCollider2D>().enabled=false;
         closedCollider.GetComponent<BoxCollider2D>().enabled=true;
+        red.enabled = true;
+        green.enabled = false;
     }
 }
