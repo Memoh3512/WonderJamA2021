8c9a32c993c70968d075e001b937efd3bab478bf
Mattys Gervais <mattysgervais@gmail.com>
2021-10-10 10:28:03 -0400

    Final Level doors work

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -6,10 +6,14 @@ public class OpenDoor : MonoBehaviour
 {
     public Sprite closed;
     public Sprite opened;
+    private GameObject closedCollider;
+    private GameObject openedCollider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        closedCollider = transform.Find("ClosedCollider").gameObject;
+        openedCollider = transform.Find("OpenedCollider").gameObject;
+        CloseSesame();
     }
 
     // Update is called once per frame
@@ -21,5 +25,16 @@ public class OpenDoor : MonoBehaviour
     {
         transform.GetComponent<SpriteRenderer>().sprite = opened;
         GameObject.FindWithTag("Player").GetComponent<PlayerControls>().lockMovement = false;
+        openedCollider.GetComponent<BoxCollider2D>().enabled=true;
+        closedCollider.GetComponent<BoxCollider2D>().enabled=false;
+        Invoke("CloseSesame",2f);
+        
+    }
+    private void CloseSesame()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = closed;
+        GameObject.FindWithTag("Player").GetComponent<PlayerControls>().lockMovement = false;
+        openedCollider.GetComponent<BoxCollider2D>().enabled=false;
+        closedCollider.GetComponent<BoxCollider2D>().enabled=true;
     }
 }
